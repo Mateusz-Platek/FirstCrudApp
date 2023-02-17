e7d2bc4894728aff07fd3f035ab7a6b676ab4538
Mateusz-Platek <m.platek1705@gmail.com>
2023-02-17 12:31:49 +0100

    Created function to insert values to table and function to create table

--- src/main/java/org/app/crud/first/DbFunctions.java
+++ src/main/java/org/app/crud/first/DbFunctions.java
@@ -2,9 +2,11 @@ package org.app.crud.first;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DbFunctions {
-    public Connection connect_to_db() {
+    public static Connection connect_to_db() {
         Connection conn = null;
         try {
             conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tutdb", "tutuser", "tutpass");
@@ -18,5 +20,32 @@ public class DbFunctions {
             System.out.println("Connected to database");
         }
         return conn;
+    }
+
+    public static void create_table(Connection conn) {
+        Statement statement;
+        try {
+            statement = conn.createStatement();
+            statement.execute(
+                    "CREATE TABLE employees (emp_id BIGSERIAL NOT NULL, name VARCHAR(150) NOT NULL, address VARCHAR(150) NOT NULL, primary key (emp_id));"
+            );
+            System.out.println("Table created");
+        }
+        catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public static void insert_row(Connection conn, String name, String address) {
+        Statement statement;
+        String query = String.format("INSERT INTO employees (name, address) VALUES ('%s', '%s');", name, address);
+        try {
+            statement = conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Row inserted");
+        }
+        catch (SQLException exception) {
+            exception.printStackTrace();
+        }
     }
 }
--- src/main/java/org/app/crud/first/Main.java
+++ src/main/java/org/app/crud/first/Main.java
@@ -1,8 +1,11 @@
 package org.app.crud.first;
 
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
-        DbFunctions db = new DbFunctions();
-        db.connect_to_db();
+        Connection conn = DbFunctions.connect_to_db();
+        //DbFunctions.create_table(conn);
+        DbFunctions.insert_row(conn, "Adam", "Cracow");
     }
 }
