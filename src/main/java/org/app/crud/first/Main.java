6598df81bac5b0d38f98b68b9328f17d44188656
Mateusz-Platek <m.platek1705@gmail.com>
2023-02-17 16:30:50 +0100

    Created menu

--- src/main/java/org/app/crud/first/Main.java
+++ src/main/java/org/app/crud/first/Main.java
@@ -1,18 +1,63 @@
 package org.app.crud.first;
 
 import java.sql.Connection;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Connection conn = DbFunctions.connect_to_db();
-        //DbFunctions.create_table(conn);
-        //DbFunctions.insert_row(conn, "Piotr", "Opole");
-        //DbFunctions.update_name(conn, "Adam", "Michal");
-        //DbFunctions.read_data(conn);
-        //DbFunctions.search_by_name(conn, "Piotr");
-        //DbFunctions.search_by_id(conn, 2);
-        //DbFunctions.delete_by_name(conn, "Piotr");
-        //DbFunctions.delete_by_id(conn, 1);
-        DbFunctions.delete_table(conn);
+        Scanner scanner = new Scanner(System.in);
+        int option;
+        String oldName;
+        String newName;
+        String address;
+        int emp_id;
+        while (true) {
+            Menu.showMenu();
+            option = Integer.parseInt(scanner.nextLine());
+            switch (option) {
+                case 1 -> DbFunctions.create_table(conn);
+                case 2 -> {
+                    System.out.println("Input name:");
+                    oldName = scanner.nextLine();
+                    System.out.println("Input address:");
+                    address = scanner.nextLine();
+                    DbFunctions.insert_row(conn, oldName, address);
+                }
+                case 3 -> DbFunctions.read_data(conn);
+                case 4 -> {
+                    System.out.println("Input old name:");
+                    oldName = scanner.nextLine();
+                    System.out.println("Input new name:");
+                    newName = scanner.nextLine();
+                    DbFunctions.update_name(conn, oldName, newName);
+                }
+                case 5 -> {
+                    System.out.println("Input name:");
+                    oldName = scanner.nextLine();
+                    DbFunctions.search_by_name(conn, oldName);
+                }
+                case 6 -> {
+                    System.out.println("Input id:");
+                    emp_id = Integer.parseInt(scanner.nextLine());
+                    DbFunctions.search_by_id(conn, emp_id);
+                }
+                case 7 -> {
+                    System.out.println("Input name:");
+                    oldName = scanner.nextLine();
+                    DbFunctions.delete_by_name(conn, oldName);
+                }
+                case 8 -> {
+                    System.out.println("Input id:");
+                    emp_id = Integer.parseInt(scanner.nextLine());
+                    DbFunctions.delete_by_id(conn, emp_id);
+                }
+                case 9 -> DbFunctions.delete_table(conn);
+                case 10 -> {
+                    System.out.println("End");
+                    System.exit(0);
+                }
+            }
+        }
     }
 }
---
+++ src/main/java/org/app/crud/first/Menu.java
@@ -0,0 +1,17 @@
+package org.app.crud.first;
+
+public class Menu {
+    public static void showMenu() {
+        System.out.println("1 - Create table");
+        System.out.println("2 - Add person");
+        System.out.println("3 - Read data from table");
+        System.out.println("4 - Update name of person");
+        System.out.println("5 - Search person by name");
+        System.out.println("6 - Search person by id");
+        System.out.println("7 - Delete person by name");
+        System.out.println("8 - Delete person by id");
+        System.out.println("9 - Delete table");
+        System.out.println("10 - Exit");
+        System.out.println("Choose option:");
+    }
+}
